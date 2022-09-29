c0a247196f23ae8d5e67cf4847b27e767282f894
Eziz1994 <orametow@mail.ru>
2022-09-29 12:28:06 +0300

    Add

--- src/FavoriteSong.java
+++ src/FavoriteSong.java
@@ -1,5 +1,6 @@
 public class FavoriteSong {
     public static void main(String[] args) {
         System.out.println("За тобой, не закрывая дверь - Я живу уже который год");
+        System.out.println("За тобой, не закрывая дверь - Я живу уже который год");
     }
 }
